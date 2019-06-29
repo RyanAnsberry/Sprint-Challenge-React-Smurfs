4859942db888e5d010e21fd1afcdb94c1297937b
Ryan Ansberry <ansman92@gmail.com>
2019-06-28 18:07:19 -0700

    Set up server data display and Post function

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
@@ -14,10 +15,27 @@ class App extends Component {
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount() {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => this.setState({ smurfs: response.data }))
+      .catch(err => console.log(err));
+  }
+
+  createSmurf = (smurf) => {
+    axios.post('http://localhost:3333/smurfs', smurf)
+       .then(response => {
+         this.setState({ smurfs: response.data })
+       })
+       .catch(err => {
+        console.log(err.response)
+       });
+  }
+  
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm createSmurf={this.createSmurf}/>
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
